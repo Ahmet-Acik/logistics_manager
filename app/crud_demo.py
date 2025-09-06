0f37d9d1ddb7c86efd0a34d46f0184f8a23ecdbe
drahmetacik <a.acik@icloud.com>
2025-09-06 10:34:07 +0100

    Add CRUD functions for route management in CRUD demo

--- app/crud_demo.py
+++ app/crud_demo.py
@@ -45,6 +45,27 @@ def delete_warehouse(session, warehouse_id):
     session.delete(warehouse)
     session.commit()
 
+def create_route(session, origin, destination, distance_km):
+    route = Route(origin=origin, destination=destination, distance_km=distance_km)
+    session.add(route)
+    session.commit()
+    return route
+
+def get_routes(session):
+    return session.query(Route).all()
+
+def update_route(session, route_id, **kwargs):
+    route = session.query(Route).get(route_id)
+    for key, value in kwargs.items():
+        setattr(route, key, value)
+    session.commit()
+    return route
+
+def delete_route(session, route_id):
+    route = session.query(Route).get(route_id)
+    session.delete(route)
+    session.commit()
+
 
 
 def main():
