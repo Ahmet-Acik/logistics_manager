f9095573fd2e1eadcb02373bfdc6a7e8acf4fc8a
drahmetacik <a.acik@icloud.com>
2025-09-13 20:14:02 +0100

    Format SQL query in running_total_orders function for improved readability

--- sql_practice_examples.py
+++ sql_practice_examples.py
@@ -204,13 +204,13 @@ def run_sql_file(filepath):
 # Example: Window function (running total)
 def running_total_orders():
     query = '''
-SELECT t1.id, t1.customer_id, t1.order_date,
-       (SELECT SUM(t2.total_amount)
-        FROM orders t2
-        WHERE t2.order_date <= t1.order_date) AS running_total
-FROM orders t1
-ORDER BY t1.order_date
-'''
+    SELECT t1.id, t1.customer_id, t1.order_date,
+        (SELECT SUM(t2.total_amount)
+            FROM orders t2
+            WHERE t2.order_date <= t1.order_date) AS running_total
+    FROM orders t1
+    ORDER BY t1.order_date
+    '''
     df = pd.read_sql(query, engine)
     show_df(df, "Running Total of Orders")
 
